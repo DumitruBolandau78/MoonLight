0780a9bda08114f4ded41dfe6e1bcdcf25649d36
Dumitru Bolandau <109096945+DumitruBolandau78@users.noreply.github.com>
2023-01-02 13:42:37 +0200

    wdsadsa

--- js/index.js
+++ js/index.js
@@ -289,15 +289,3 @@ const form = document.querySelector('form.form');
 form.addEventListener('click', (e) => {
     e.preventDefault;
 });
-
-
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-// We listen to the resize event
-window.addEventListener('resize', () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
