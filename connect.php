7fd7af2dd52dedcbbfe042fad5841e53d1634212
Dumitru Bolandau <109096945+DumitruBolandau78@users.noreply.github.com>
2023-01-18 22:35:29 +0200

    update

---
+++ connect.php
@@ -0,0 +1,20 @@
+<?php
+
+   $connection = mysqli_connect('localhost','root','','book_db');
+
+   if(isset($_POST['send'])){
+      $name = $_POST['name'];
+      $email = $_POST['email'];
+      $subject = $_POST['subject'];
+      $message = $_POST['message'];
+
+      $request = " insert into connect(name, email, subject, message) values('$name','$email','$subject','$message')";
+      mysqli_query($connection, $request);
+
+      header('location:connect.php');
+
+   }else{
+      echo 'something went wrong please try again!';
+   }
+
+?>
